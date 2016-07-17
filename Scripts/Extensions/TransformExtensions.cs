1d3a947e82e6a0170268da50f8da30c5d5cb508e
Simon Rodriguez <simon@hellowhiteout.com>
2016-07-17 21:46:30 +0200

    Added version of DeleteChildren where you can set the startIndex, to skip the first one e.g

--- Scripts/Extensions/TransformExtensions.cs
+++ Scripts/Extensions/TransformExtensions.cs
@@ -8,4 +8,10 @@ public static class TransformExtensions
 		for (int i = transform.childCount - 1; i >= 0; i--)
 			transform.GetChild(i).gameObject.DeleteEditorSafe(detach);
 	}
+
+	public static void DeleteAllChildren(this Transform transform, int start, bool detach = false)
+	{
+		for (int i = transform.childCount - 1; i >= start; i--)
+			transform.GetChild(i).gameObject.DeleteEditorSafe(detach);
+	}
 }

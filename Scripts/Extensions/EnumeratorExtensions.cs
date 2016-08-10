52a47a269f1678bed6c5ed575ae824a24804f49f
Simon Rodriguez <simon@hellowhiteout.com>
2016-08-10 14:46:31 +0200

    Extension for IEnumerator, for running a coroutine directly.

---
+++ Scripts/Extensions/EnumeratorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+public static class EnumeratorExtensions
+{
+	public static void RunNow(this IEnumerator enumerator)
+	{
+		while (enumerator.MoveNext())
+		{
+			
+		}
+	}
+}

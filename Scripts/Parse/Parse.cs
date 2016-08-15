0ddcf5dc47c4b2b892efdebbbfc5f19031028031
Simon Rodriguez <simon@hellowhiteout.com>
2016-08-15 15:37:04 +0200

    Parse REST API - Only supports calling cloud functions.

---
+++ Scripts/Parse/Parse.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[Serializable]
+public class ParseRequest
+{
+
+}
+
+[Serializable]
+public class ParseResponse
+{
+
+}
+
+[Serializable]
+public class ParseErrorResponse
+{
+	public int code = 0;
+	public string error = null;
+}
+
+public class Parse
+{
+	class ParseDummy : MonoBehaviour
+	{
+		
+	}
+
+	class ParseSettings : ScriptableObject
+	{
+		[Multiline]
+		public string AppId = "null";
+		[Multiline]
+		public string RESTKey = "null";
+		[Multiline]
+		public string Host = "null";
+	}
+
+	//Does not change, create in constructor and mark read-only.
+	readonly Dictionary<string, string> Headers;
+
+	readonly ParseDummy behaviour = null;
+
+	readonly ParseSettings settings = null;
+
+	private Parse()
+	{
+		settings = Resources.Load<ParseSettings>("Parse");
+
+		if (settings == null)
+		{
+			settings = ScriptableObject.CreateInstance<ParseSettings>();
+
+			#if UNITY_EDITOR
+
+			AssetDatabase.CreateAsset(settings, "Assets/Resources/Parse.asset");
+			AssetDatabase.SaveAssets();
+
+			Debug.LogError("Fill in your information in Resources/Parse.asset");
+
+			#endif
+		}
+
+		GameObject go = new GameObject();
+		go.name = "[Parse]";
+		behaviour = go.AddComponent<ParseDummy>();
+		GameObject.DontDestroyOnLoad(go);
+
+		Headers = new Dictionary<string, string>()
+		{
+			{ "X-Parse-Application-Id", settings.AppId },
+			{ "X-Parse-REST-API-Key", settings.RESTKey },
+			{ "Content-Type", "application/json" }
+		};
+	}
+
+	public YieldInstruction CloudCode<T_Request, T_Response>(string function, T_Request req, System.Action<T_Response> onComplete, System.Action<ParseErrorResponse> onError = null) 
+		where T_Request : ParseRequest
+		where T_Response : ParseResponse, new()
+	{
+		return behaviour.StartCoroutine(CoRequest<T_Request, T_Response>(function, req, onComplete, onError));
+	}
+
+	IEnumerator CoRequest<T_Request, T_Response>(string function, T_Request req, System.Action<T_Response> onComplete, System.Action<ParseErrorResponse> onError)
+		where T_Request : ParseRequest
+		where T_Response : ParseResponse, new()
+	{
+		string json = JsonUtility.ToJson(req);
+
+		Debug.Log("[Parse] Request to '" + function + "' with data: " + json);
+
+		WWW www = new WWW(settings.Host + "/functions/" + function, Encoding.UTF8.GetBytes(json), Headers);
+
+		yield return www;
+
+		if (!String.IsNullOrEmpty(www.error))
+		{
+			Debug.LogWarning("[Parse] Error from '" + function + "' with data: " + www.text);
+
+			if (www.text.StartsWith("{"))
+			{
+				ParseErrorResponse resp = JsonUtility.FromJson<ParseErrorResponse>(www.text);
+
+				if (onError != null)
+					onError(resp);
+			}
+			else
+			{
+				ParseErrorResponse resp = new ParseErrorResponse{ code = 0, error = "No answer from server." };
+
+				if (onError != null)
+					onError(resp);
+			}
+		}
+		else
+		{
+			Debug.Log("[Parse] Response from '" + function + "' with data: " + www.text);
+
+			T_Response resp = JsonUtility.FromJson<T_Response>(www.text);
+
+			if (onComplete != null)
+				onComplete(resp);
+		}
+	}
+
+	#region SINGLETON
+
+	static Parse instance = null;
+
+	public static Parse Instance
+	{
+		get
+		{
+			if (instance == null)
+				instance = new Parse();
+
+			return instance;
+		}
+	}
+
+	#endregion
+}
+
+/*
+[Serializable]
+public class ParseRequestList : ParseRequest
+{
+	public string id;
+}
+
+[Serializable]
+public class ParseResponseList : ParseResponse
+{
+	[Serializable]
+	public class ParseResponseListItem
+	{
+		public string gameId;
+	}
+
+	public ParseResponseListItem[] result;
+}
+
+Example1,
+
+ParseRequestList req = new ParseRequestList(){ id = "uhALEnmaaZ" };
+
+Instance.CloudCode<ParseRequestList, ParseResponseList>("list", req, (result) =>
+	{
+		Log.D(result.result[0].gameId);
+	}, null);
+
+Example2,
+
+ParseRequestList req = new ParseRequestList(){ id = "uhALEnmaaZ" };
+
+ParseResponseList resp = null;
+
+yield return Parse.Instance.CloudCode<ParseRequestList, ParseResponseList>("list", req, (result) =>
+	{
+		resp = result;
+	});
+
+if (resp != null)
+	Debug.Log("answer " + resp.result[0].gameId);
+else
+	Debug.LogWarning("error");
+*/

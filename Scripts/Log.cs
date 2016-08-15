251a3c73f9ca68d799f4b69ac59e7837338b5787
Simon Rodriguez <simon@hellowhiteout.com>
2016-08-15 15:35:04 +0200

    Removed functions that wasn't used.

--- Scripts/Log.cs
+++ Scripts/Log.cs
@@ -30,32 +30,3 @@ public static class Log
 		Debug.LogWarning(sb.ToString());
 	}
 }
-
-public static class LogExtensions
-{
-	public static void Trace(this System.Object obj, params object[] vars)
-	{
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < vars.Length; i++)
-		{
-			sb.Append(vars[i] == null ? "(null)" : vars[i]);
-			sb.Append(" ");
-		}
-
-		Debug.Log(sb.ToString());
-	}
-
-	public static void Trace(this UnityEngine.Object mono, params object[] vars)
-	{
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < vars.Length; i++)
-		{
-			sb.Append(vars[i] == null ? "(null)" : vars[i]);
-			sb.Append(" ");
-		}
-
-		Debug.Log(sb.ToString(), mono);
-	}
-}

d9366b02815a93d9cdc714a6b90efdae4d35d451
Simon Rodriguez <simon@hellowhiteout.com>
2018-12-01 17:48:52 +0100

    Latest Log functions with F that shows the function it was called from. Also conditionals to remove logging from builds

--- Scripts/Log.cs
+++ Scripts/Log.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
-using System;
+﻿using System;
 using System.Text;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 public static class Log
 {
+	[Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
 	public static void D(params object[] vars)
 	{
 		StringBuilder sb = new StringBuilder();
@@ -14,9 +16,10 @@ public static class Log
 			sb.Append(" ");
 		}
 
-		Debug.Log(sb.ToString());
+		UnityEngine.Debug.Log(sb.ToString());
 	}
 
+	[Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
 	public static void W(params object[] vars)
 	{
 		StringBuilder sb = new StringBuilder();
@@ -27,6 +30,45 @@ public static class Log
 			sb.Append(" ");
 		}
 
-		Debug.LogWarning(sb.ToString());
+		UnityEngine.Debug.LogWarning(sb.ToString());
+	}
+
+	[Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
+	public static void E(params object[] vars)
+	{
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < vars.Length; i++)
+		{
+			sb.Append(vars[i] == null ? "(null)" : vars[i]);
+			sb.Append(" ");
+		}
+
+		UnityEngine.Debug.LogError(sb.ToString());
+	}
+
+	[Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public static void F(params object[] vars)
+	{
+		StackTrace st = new StackTrace();
+		StackFrame sf = st.GetFrame(1);
+
+		StringBuilder sb = new StringBuilder();
+		sb.Append(sf.GetMethod().DeclaringType);
+		sb.Append(".");
+		sb.Append(sf.GetMethod().Name);
+		sb.Append("(");
+
+		for (int i = 0; i < vars.Length; i++)
+		{
+			sb.Append(vars[i] == null ? "(null)" : vars[i]);
+
+			if (i < vars.Length - 1)
+				sb.Append(", ");
+		}
+		sb.Append(")");
+
+		UnityEngine.Debug.Log(sb.ToString());
 	}
 }
